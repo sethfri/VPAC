37ef25f6505eb832e2388cffbed1fbfa133751bc
Seth Friedman <seth.n.friedman@vanderbilt.edu>
2013-12-21 02:30:07 -0600

    Remove double quotes from static page specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,59 +1,59 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe 'Static pages' do
 
   subject { page }
 
-  describe "Home page" do
+  describe 'Home page' do
     before { visit root_path }
 
     it { should have_title('Vanderbilt Performing Arts Council') }
     it { should_not have_title('| Home') }
   end
 
-  describe "Mission page" do
+  describe 'Mission page' do
     before { visit mission_path }
 
     it { should have_content('Mission') }
     it { should have_title('Vanderbilt Performing Arts Council | Mission') }
   end
 
-  describe "History page" do
+  describe 'History page' do
     before { visit history_path }
 
     it { should have_content('History') }
     it { should have_title('Vanderbilt Performing Arts Council | History') }
   end
 
-  describe "Calendar page" do
+  describe 'Calendar page' do
     before { visit calendar_path }
 
     it { should have_content('Calendar') }
     it { should have_title('Vanderbilt Performing Arts Council | Calendar') }
   end
 
-  describe "Spotlight page" do
+  describe 'Spotlight page' do
     before { visit spotlight_path }
 
     it { should have_content('Spotlight') }
     it { should have_title('Vanderbilt Performing Arts Council | Spotlight') }
   end
 
-  describe "Main Attraction page" do
+  describe 'Main Attraction page' do
     before { visit main_attraction_path }
 
     it { should have_content('Main Attraction') }
     it { should have_title('Vanderbilt Performing Arts Council | Main Attraction') }
   end
 
-  describe "Encore page" do
+  describe 'Encore page' do
     before { visit encore_path }
 
     it { should have_content('Encore') }
     it { should have_title('Vanderbilt Performing Arts Council | Encore') }
   end
 
-  describe "Contact page" do
+  describe 'Contact page' do
     before { visit contact_path }
 
     it { should have_content('Contact') }
