cfe16a91a35da79586b6f64178d01abe03bc1353
Seth Friedman <seth.n.friedman@vanderbilt.edu>
2014-04-19 07:37:33 -0500

    Properly import the attendance spreadsheets

--- app/models/attended_show.rb
+++ app/models/attended_show.rb
@@ -3,13 +3,19 @@ class AttendedShow < ActiveRecord::Base
   belongs_to :member_group
 
   def self.import(title, host_org, file)
-    show = AttendedShow.create(title: title, host_org: host_org, school_year: '2013-2014')
+    group_name = MemberGroup.find(host_org).name
 
-    CSV.foreach(file.path, headers: true) do |row|
-      email = row['Campus Email']
+    MemberGroup.all.each do |group|
+      if (!group.name.eql?(group_name))
+        show = AttendedShow.new(title: title, host_org: group_name, school_year: '2013-2014', member_group: group)
 
-      member = Member.find_by email: email.downcase
-      show.members.push(member) unless member.nil?
+        CSV.foreach(file.path, headers: true) do |row|
+          email = row['Campus Email']
+
+          member = Member.find_by email: email.downcase
+          member.attended_shows.push(show) unless member.nil? || !member.member_groups.exists?(group)
+        end
+      end
     end
   end
 end
